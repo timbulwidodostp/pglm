657fc9967fc3b8e02e7a5db82fc585152fdec370
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-04-14 23:01:51 +0700

    Use Package pglm

--- pglm/pglm.R
+++ pglm/pglm.R
@@ -10,20 +10,20 @@ library("httr")
 library("readxl")
 library("pglm")
 # Import Data Excel Into R From Github Olah Data Semarang (timbulwidodostp)
-github_link <- "https://github.com/timbulwidodostp/pglm/raw/main/pglm/pglm.xlsx"
+github_link <- "https://github.com/timbulwidodostp/pglm/raw/main/pglm/ships.xlsx"
 temp_file <- tempfile(fileext = ".xlsx")
 req <- GET(github_link, 
 # authenticate using GITHUB_PAT
 authenticate(Sys.getenv("GITHUB_PAT"), ""),
 # write result to disk
 write_disk(path = temp_file))
-pglm <- readxl::read_excel(temp_file)
+ships <- readxl::read_excel(temp_file)
 # Estimate Fixed Effect Poisson Model Use Package pglm With (In) R Software
 res <- pglm(accident ~ op_75_79+co_65_69+co_70_74+co_75_79+lnservice,family = poisson, 
-data = pglm, effect = "twoways", model="within", index = c("ship", "time"))
+data = ships, effect = "twoways", model="within", index = c("ship", "time"))
 summary(res)
 res <- pglm(accident ~ op_75_79+co_65_69+co_70_74+co_75_79+lnservice,family = poisson, 
-data = pglm, effect = "individual", model="within", index = c("ship"))
+data = ships, effect = "individual", model="within", index = c("ship"))
 summary(res)
 # Fixed Effect Poisson Model Use Package pglm With (In) R Software
 # Olah Data Semarang
